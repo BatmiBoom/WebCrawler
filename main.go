07ab56e022db790402ce6dd784a733deadf207f5
Nicolas Stirnemann <nstirnemann59@gmail.com>
2024-09-10 20:03:31 -0300

    Main

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	arg_len := len(os.Args[1:])
+	if arg_len < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if arg_len > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	BASE_URL := os.Args[1]
+	fmt.Printf("starting crawl of: %s", BASE_URL)
 }
